ba94e76109ae27e4df51340bdf84675421d26a67
Carolina <ribeiro.carolribeiro.carolina@gmail.com>
2021-05-26 18:06:08 -0300

    Projeto Blog Pessoal Parte 1

---
+++ BlogPessoal-1/src/test/java/br/com/generation/blog/pessoal/BlogPessoal1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.generation.blog.pessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPessoal1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
