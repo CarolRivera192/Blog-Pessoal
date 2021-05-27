6cc0764e385b8b236e0a345a01ef7c387ecc7914
Carolina <ribeiro.carolribeiro.carolina@gmail.com>
2021-05-27 17:21:20 -0300

    Projeto Blog Pessoal Parte 2

--- BlogPessoal-1/src/main/java/br/com/generation/blog/pessoal/controller/PostagemController.java
+++ BlogPessoal-1/src/main/java/br/com/generation/blog/pessoal/controller/PostagemController.java
@@ -3,16 +3,20 @@ package br.com.generation.blog.pessoal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.generation.blog.pessoal.model.Postagem;
 import br.com.generation.blog.pessoal.repository.PostagemRepository;
-
 
 @RestController
 @RequestMapping ("/postagens")
@@ -39,5 +43,23 @@ public class PostagemController {
 	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem> post (@RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+				
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> put (@RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+				
+	}
+	
+	@DeleteMapping ("/{id}")
+	public void delete(@PathVariable long id) {// Void: não retorna nada 
+		repository.deleteById(id);
+	}
+	
 
 }
