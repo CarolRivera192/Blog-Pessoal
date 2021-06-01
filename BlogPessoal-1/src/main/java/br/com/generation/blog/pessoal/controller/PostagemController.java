6776dc2153178a1862f6d47d0f3122e3765df3e1
Carolina <ribeiro.carolribeiro.carolina@gmail.com>
2021-06-01 13:55:18 -0300

    Colocando comentários no código

--- BlogPessoal-1/src/main/java/br/com/generation/blog/pessoal/controller/PostagemController.java
+++ BlogPessoal-1/src/main/java/br/com/generation/blog/pessoal/controller/PostagemController.java
@@ -18,45 +18,55 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.generation.blog.pessoal.model.Postagem;
 import br.com.generation.blog.pessoal.repository.PostagemRepository;
 
-@RestController
-@RequestMapping ("/postagens")
-@CrossOrigin (origins = "*", allowedHeaders = "*")
+@RestController //Indica que a classe é a controller(Recebe qualquer requisição HTTP e vai responde-las de acordo com os verbos)
+@RequestMapping ("/postagens") //Indica um endpoint que vai ser acessado por essa classe(URL)
+@CrossOrigin (origins = "*", allowedHeaders = "*") //Diz que a classe aceita requesições de qualquer origem (Front end)
 public class PostagemController {
 	
-	@Autowired //Injeção de Dependência
-	private PostagemRepository repository;
+	@Autowired //Injeção de Dependência, passa o controle para o repository(cria e instancia objetos)
+	private PostagemRepository repository;// acesso ao banco de dados
 
+	//===================== Find All ========================//
+	
 	@GetMapping
 	public ResponseEntity<List<Postagem>> GetAll() {
 		return ResponseEntity.ok(repository.findAll());
 	}
 
+	//=================== Find By Id ======================//
+	
 	@GetMapping ("/{id}")
-	public ResponseEntity<Postagem> GetById(@PathVariable long id) {
+	public ResponseEntity<Postagem> GetById(@PathVariable long id) { //PathVariable= variavel do caminho(URL) que pega o número do id
 		return repository.findById(id)
-				.map(resp -> ResponseEntity.ok(resp))
+				.map(resp -> ResponseEntity.ok(resp)) //ResponseEntity = resposta que ai returna
 				.orElse(ResponseEntity.notFound().build());
 
 	}
+	
+	//=================== Find By Titulo ======================//
 
-	@GetMapping ("/titulo/{titulo}")
+	@GetMapping ("/titulo/{titulo}") //O que esta entre chaves é uma variavel 
 	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 	}
 	
+	//=================== Post Postagem ======================//
 	@PostMapping
 	public ResponseEntity<Postagem> post (@RequestBody Postagem postagem) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
 				
 	}
 	
+	//=================== Put Postagem ======================//
 	@PutMapping
 	public ResponseEntity<Postagem> put (@RequestBody Postagem postagem) {
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
 				
 	}
 	
-	@DeleteMapping ("/{id}")
+	//=================== Delete Postagem ======================//
+	
+	@DeleteMapping ("/{id}") //O que esta entre chaves é uma variavel 
 	public void delete(@PathVariable long id) {// Void: não retorna nada 
 		repository.deleteById(id);
 	}
